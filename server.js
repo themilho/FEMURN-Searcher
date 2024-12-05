9cbf563f2c06064c6b1b28ee0ec17935b5b131f9
themilho <the.milho@outlook.com>
2024-12-04 21:52:51 -0300

    Ajustes no vercel.json para que o vercel instale as dependências automaticamente

--- server.js
+++ server.js
@@ -1,16 +1,3 @@
-// const app = require('express')();
-// const bodyParser = require('body-parser');
-
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// app.get('/', (req, res) => {
-//     res.send('Hello, Vercel!');
-// });
-
-// module.exports = app; // Exporta o app
-
-
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const puppeteer = require('puppeteer');
